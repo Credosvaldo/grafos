e66007a8e86a9081bf12971a1387ae76b11d67f7
Pedro Henrique Pires Rodrigues <67903358+Credosvaldo@users.noreply.github.com>
2024-10-06 23:57:34 -0300

    feat: mudando o projeto para python

---
+++ Grafo.py
@@ -0,0 +1,7 @@
+class Grafo: 
+    
+    def __init__(self) -> None:
+        self.components_number = 4
+        
+        
+        
---
+++ main.py
@@ -0,0 +1,8 @@
+from Grafo import Grafo
+
+
+print('Hello World')
+
+a = Grafo()
+
+print(a.components_number)
