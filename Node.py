4b6d544e42f76f22a304f234f7ba7aa1a06939bd
Vinicius Rezende <viniciusarantes133@gmail.com>
2024-11-20 15:44:27 -0300

    feat: implementação da classe GrafoLA e métodos para manipulação de nós e arestas

--- Node.py
+++ Node.py
@@ -1,10 +1,10 @@
 class Node:
-    def __init__(self, index: int, weight: float = 0):
+    def __init__(self, index: int, weight: float = None):
         self.index = index
         self.weight = weight
-        
+
     def _str_(self) -> str:
         return str(self.index)
-    
+
     def __repr__(self) -> str:
         return self._str_()
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from DFSNode import DFSNode
 from Grafo import GrafoMA
+from GrafoLA import GrafoLA
 
 a = GrafoMA(DIRECTED=True)
 
@@ -10,4 +11,20 @@ a.add_edge(2, 1, 6)
 a.add_edge(3, 2, 5)
 
 print(a)
-print(a.get_articulations())
+
+b = GrafoLA(DIRECTED=False)
+
+b.add_node("1", 7)
+b.add_node("2", 8)
+b.add_node("3", 9)
+
+b.add_edge("1", "2", 7, "1-2")
+b.add_edge("3", "2", 8, "3-2")
+print(b)
+
+b.remove_node("2")
+print(b)
+
+
+c = GrafoLA(DIRECTED=True, num_nodes=12)
+print(c)
